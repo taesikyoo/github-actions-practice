f636090a2a65362591c0df65d200a845e54c827f
taesikyoo <yrns0907@gmail.com>
2020-07-23 22:23:40 +0900

    github actions로 build하기

---
+++ src/test/java/com/taesikyoo/practice/githubactions/controller/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.taesikyoo.practice.githubactions.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
