d481e220c5848323980dceb2deb42dd57a3a9400
taesikyoo <yrns0907@gmail.com>
2020-07-23 20:52:40 +0900

    first commit

---
+++ src/test/java/com/taesikyoo/practice/githubactions/GithubActionsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taesikyoo.practice.githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
